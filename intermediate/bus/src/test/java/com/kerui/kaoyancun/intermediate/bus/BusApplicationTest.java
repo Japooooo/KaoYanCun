a5aa3a2135613d7a0a44c3df1e0862516132461f
Japooooo <admin@jurvs.com>
2019-10-18 14:49:35 +0800

    first commit

---
+++ intermediate/bus/src/test/java/com/kerui/kaoyancun/intermediate/bus/BusApplicationTest.java
@@ -0,0 +1,20 @@
+package com.kerui.kaoyancun.intermediate.bus;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple BusApplication.
+ */
+public class BusApplicationTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
